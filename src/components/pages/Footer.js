d893adea748111f10c254e4e89d817c8b0828034
sandymohamede2 <sandymohamede2@gmail.com>
2023-09-03 06:06:13 +0300

    edit footer

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -9,68 +9,57 @@ const Footer = () => {
         <section className='footer-section'>
             <div className='footer'>
 
-            <div className='footer-logo'>
-                <p rel="nofollow" href="index.html">
+                <div className='footer-logo'>
                     <img src={logo} alt="geeks" />
-                 </p>
 
-                <p> 
-                    447 Broadway, 2nd Floor Suite #772, New York 10013, United States
-                    <br /> 
-                    +1-347-535-0004 
+
+                    <p>
+                        447 Broadway, 2nd Floor Suite #772, New York 10013, United States
+                        <br />
+                        +1-347-535-0004
                     </p>
-            </div>
-
-            <nav>
-                <ul className="clearfix">
-                    <li>
-                        <p rel="nofollow" href="works/">
-                            <span>Works</span>
-                         </p>
-                    </li>
-                    <li>
-                        <p rel="nofollow" href="technologies/">
-                            <span>Technologies</span>
-                         </p>
-                    </li>
-                    <li>
-                        <p rel="nofollow" href="services/">
-                            <span>Services</span>
-                         </p>
-                    </li>
-                    <li>
-                        <p rel="nofollow" href="about/">
-                            <span>Company</span>
-                         </p>
-                    </li>
-                    <li>
-                        <p rel="nofollow" href="contacts/">
-                            <span>Contacts</span>
-                         </p>
-                    </li>
-                    <li>
-                        <p href="terms.html">
-                        <span>Terms and Conditions</span>
-                         </p>
-                        <p href="privacy.html">
-                        <span>Privacy Policy</span>
-                         </p>
-                    </li>
-                </ul>
-            </nav>
-
-            <p rel="nofollow"
-                href="#"
-                className=" btn-white subscription-dialog"
-                data-toggle="modal"
-                data-target="#subscribeModal"
-                data-backdrop="static"
-                >Stay tuned for our updates </p>
-            
                 </div>
 
-                <p className='muted-text'>© Copyright 2023 – Mobile App Design and Development Company. All Rights Reserved</p>
-            </section>
+                <nav>
+                    <ul className="clearfix">
+                        <li>
+
+                            <span>Works</span>
+
+                        </li>
+                        <li>
+                            <span>Technologies</span>
+                        </li>
+                        <li>
+                            <span>Services</span>
+                        </li>
+                        <li>
+                            <span>Company</span>
+                        </li>
+                        <li>
+                            <span>Contacts</span>
+                        </li>
+                        <li>
+                            <span>Terms and Conditions</span>
+                        </li>
+                        <li>
+                            <span>Privacy Policy</span>
+                        </li>
+                    </ul>
+                </nav>
+
+                <p
+                    className=" btn-white subscription-dialog"
+                    data-toggle="modal"
+                    data-target="#subscribeModal"
+                    data-backdrop="static"
+                >Stay tuned for our updates
+                </p>
+
+            </div>
+
+            <p className='muted-text'>© Copyright 2023 – Mobile App Design and Development Company. All Rights Reserved</p>
+        </section>
 
     )
 }
