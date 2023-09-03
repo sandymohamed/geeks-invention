8ae265dc1b13cef5f73fa2e8ca6423198f0aff01
sandymohamede2 <sandymohamede2@gmail.com>
2023-09-03 05:56:33 +0300

    edit links

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,14 +19,14 @@ const Header = () => {
         setNavClass(!navClass)
     }
 
-  
+
 
     return (
         <div className='header'>
             <div className='header-container'>
                 <p rel="nofollow" href="index.html" className='logo-img'>
                     <img src={logo} alt="geeks" />
-                 </p>
+                </p>
 
                 <p href="#" className={`device-menu ${menuClass ? 'open' : ''} `} onClick={() => handleMenueClick()}>
 
@@ -35,33 +35,40 @@ const Header = () => {
                         <span className='sc'></span>
                         <span className='td'></span>
                     </div>
-                 </p>
+                </p>
 
 
                 <ul className={`nav ${navClass ? 'active-nav' : ''} `}>
 
-                    <li className={activeLink === 'home' ? 'active' : ''} >
-                        <p className='one'   onClick={() => handleClick('home')}>home  </p>
+                    <li
+                        className={`${activeLink === 'home' ? 'active' : ''} one`}
+                        onClick={() => handleClick('home')}
+                    > home
                     </li>
 
-                    <li className={activeLink === 'works' ? 'active' : ''}>
-                        <p className='two'   onClick={() => handleClick('works')}> works  </p>
+                    <li
+                        className={`${activeLink === 'works' ? 'active' : ''} two`}
+                        onClick={() => handleClick('works')}> works
                     </li>
 
-                    <li className={activeLink === 'technologies' ? 'active' : ''}>
-                        <p className='one'   onClick={() => handleClick('technologies')}>technologies  </p>
+                    <li
+                        className={`${activeLink === 'technologies' ? 'active' : ''} one`}
+                        onClick={() => handleClick('technologies')}>technologies
                     </li>
 
-                    <li className={activeLink === 'services' ? 'active' : ''}>
-                        <p className='two'   onClick={() => handleClick('services')}>services  </p>
+                    <li
+                        className={`${activeLink === 'services' ? 'active' : ''} two`}
+                        onClick={() => handleClick('services')}>services
                     </li>
 
-                    <li className={activeLink === 'compan' ? 'active' : ''}>
-                        <p className='one'   onClick={() => handleClick('compan')}>compan  </p>
+                    <li
+                        className={`${activeLink === 'compan' ? 'active' : ''} one`}
+                        onClick={() => handleClick('compan')}>compan
                     </li>
 
-                    <li className={activeLink === 'contacts' ? 'active' : ''}>
-                        <p className='two'   onClick={() => handleClick('contacts')}>contacts  </p>
+                    <li
+                        className={`${activeLink === 'contacts' ? 'active' : ''} two`}
+                        onClick={() => handleClick('contacts')}>contacts
                     </li>
 
                 </ul>
